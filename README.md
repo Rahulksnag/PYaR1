3cfdecab1e285a2dd301ec60eadb49ef7423f35f
Rahulksnag <rahulksnag@gmail.com>
2023-08-30 00:39:02 +0530

    else

---
+++ README.md
@@ -0,0 +1,2 @@
+# PYaR
+HELLO welcome
---
+++ index copy.js
@@ -0,0 +1,10 @@
+function redirectToDashboard() {
+
+    // Redirect to the dashboard only if both username and password are filled
+    window.location.href = "dashboard.html";
+
+}
+function redirectToSellerDashboard() {
+    // Replace the window.location.href with the correct path to your dashboard.html in the customer folder
+    window.location.href = "dashboardseller.html";
+  }
